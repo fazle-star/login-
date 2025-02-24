75b6c23a446000f67136c49923753433acfa7643
muhammad fazle ferino <m.fazle2006@gmail.com>
2025-02-24 20:04:48 +0700

    tugas mas ari

---
+++ src/js/logoinput.js
@@ -0,0 +1,24 @@
+function focusInput() {
+    document.getElementById('myInput').focus();
+}
+function focusInput2() {
+    document.getElementById('myInput2').focus();
+}
+function focusInput3() {
+    document.getElementById('myInput3').focus();
+}
+function focusInput4() {
+    document.getElementById('myInput4').focus();
+}
+function focusInput5() {
+    document.getElementById('myInput5').focus();
+}
+function focusInput6() {
+    document.getElementById('myInput6').focus();
+}
+function focusInput7() {
+    document.getElementById('myInput7').focus();
+}
+function focusInput8() {
+    document.getElementById('myInput8').focus();
+}
